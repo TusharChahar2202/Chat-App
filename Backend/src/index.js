6833078d4f66a12805ad8631a8e4c84c5d76286f
Tushar Chahar <161971457+TusharChahar2202@users.noreply.github.com>
2025-06-15 00:19:51 +0530

    Mongoose Connection

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,8 @@
 import express from  "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
 dotenv.config();
 
 const app = express();
@@ -11,4 +13,5 @@ app.use("/api/auth" , authRoutes)
 
 app.listen(5001 , () =>{
     console.log("Server is Running on Port " + PORT);
+    connectDB()
 });
